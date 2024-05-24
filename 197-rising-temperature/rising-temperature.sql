ff445f77031a0a0cbda55fa52552e0e213272c4c
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2024-05-24 14:34:08 -0400

    Time: 605 ms (38.83%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,5 +1,3 @@
--- Write your PostgreSQL query statement below
-
 select w2.id
-from weather w1, weather w2
-where w2.recordDate - w1.recordDate = 1 and w2.temperature > w1.temperature
+from weather w1,weather w2
+where w2.recorddate - w1.recorddate = 1 and w2.temperature > w1.temperature
