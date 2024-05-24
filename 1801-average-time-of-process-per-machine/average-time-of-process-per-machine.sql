066b514e8c73bb8c5eb5e22a3616e371b19da17b
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2024-05-24 15:37:15 -0400

    Time: 473 ms (39.25%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,18 +1,5 @@
--- Write your PostgreSQL query statement below
-
-with machine_start as (
-    select machine_id, process_id, timestamp as timestamp_start
-    from activity
-    where activity_type = 'start'
-), 
-machine_end as (
-    select machine_id, process_id, timestamp as timestamp_end
-    from activity
-    where activity_type = 'end'
-)
-
-select ms.machine_id, round(avg(me.timestamp_end - ms.timestamp_start)::numeric, 3) as processing_time 
-from machine_start ms
-join machine_end me on ms.machine_id = me.machine_id and    
-                       ms.process_id = me.process_id
-group by ms.machine_id
+select a.machine_id, round(avg(b.timestamp - a.timestamp)::numeric, 3) as processing_time
+from activity a, activity b
+where a.machine_id = b.machine_id and a.process_id = b.process_id and a.activity_type = 'start'
+and b.activity_type = 'end'
+group by a.machine_id
