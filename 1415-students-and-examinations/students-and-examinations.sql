31ca544042218702168af343fe8cf14d0a67c495
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2024-05-24 16:04:22 -0400

    Time: 957 ms (61.24%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -1,11 +1,9 @@
--- Write your PostgreSQL query statement below
-
 with all_subject as(
     select st.student_id, st.student_name, su.subject_name
     from students st, subjects su
 )
 
-select a.student_id, a.student_name, a.subject_name, count(e.student_id) as attended_exams
+select a.student_id, a.student_name, a.subject_name, count(e.subject_name) as attended_exams
 from all_subject a
 left join examinations e on a.student_id = e.student_id and a.subject_name = e.subject_name
 group by a.student_id, a.student_name, a.subject_name
