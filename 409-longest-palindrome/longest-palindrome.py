0c4945627faf8cebe9ebe6d05f01674d6158542e
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2024-06-04 12:44:10 -0400

    Time: 39 ms (41.59%) | Memory: 16.6 MB (34.50%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.py
@@ -0,0 +1,32 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        char_dict = {}
+        for char in s:
+            if char not in char_dict:
+                char_dict[char] = 1
+            else:
+                char_dict[char] += 1
+        
+        even_num = 0
+        one_count = 0
+        odd_count = []
+
+        for num in char_dict.values():
+            if num % 2 == 0:
+                even_num += num
+            else:
+                if num == 1 and one_count != 0:
+                    one_count += 1
+                else:
+                    odd_count.append(num)
+
+        if odd_count:
+            final = even_num + one_count + max(odd_count)
+            odd_count.remove(max(odd_count))
+        else:
+            final = even_num + one_count
+
+        for odd_num in odd_count:
+            final += odd_num - 1
+
+        return final
