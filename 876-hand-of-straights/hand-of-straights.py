da6bb56c17876b161502f8a8b380607b8457eeda
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2024-06-06 01:30:00 -0400

    Time: 154 ms (86.35%) | Memory: 18 MB (94.17%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.py
+++ 876-hand-of-straights/hand-of-straights.py
@@ -1,16 +1,14 @@
-from collections import Counter
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
         if len(hand) % groupSize != 0:
             return False
 
-        card_count = Counter(hand)
-        # card_count = {}
-        # for num in hand:
-        #     if num not in card_count:
-        #         card_count[num] = 1
-        #     else:
-        #         card_count[num] += 1
+        card_count = {}
+        for num in hand:
+            if num not in card_count:
+                card_count[num] = 1
+            else:
+                card_count[num] += 1
 
         unique_cards = sorted(card_count.keys())
         
@@ -18,8 +16,11 @@ class Solution:
             if card_count[card] > 0:
                 num_groups = card_count[card]
                 for i in range(groupSize):
-                    if card_count[card + i] < num_groups:
+                    if card + i not in card_count.keys():
                         return False
-                    card_count[card + i] -= num_groups
+                    else:
+                        if card_count[card + i] < num_groups:
+                            return False
+                        card_count[card + i] -= num_groups
         
         return True
