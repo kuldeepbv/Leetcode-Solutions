9f1674612f885deb4d4bedda61982d302c4669a7
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2024-06-07 17:36:55 -0400

    Time: 181 ms (38.25%) | Memory: 24.8 MB (94.74%) - LeetSync

---
+++ 648-replace-words/replace-words.py
@@ -0,0 +1,12 @@
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        sentence_list = sentence.split(' ')
+        dictionary.sort()
+
+        for i, word in enumerate(sentence_list):
+            for check_word in dictionary:
+                if word.startswith(check_word):
+                    sentence_list[i] = check_word
+                    break
+
+        return ' '.join(sentence_list)
