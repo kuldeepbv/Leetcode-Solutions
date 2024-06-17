7a6fb23d87ac7a380bec165f7d135f265331c181
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2024-06-17 13:06:52 -0400

    Time: 83 ms (80.51%) | Memory: 16.5 MB (79.87%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.py
@@ -0,0 +1,17 @@
+import math
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        l = 0
+        r = math.ceil(math.sqrt(c))
+
+        while l <= r:
+            curr_sum = (l * l) + (r * r)
+            if curr_sum == c:
+                return True
+                break
+            elif curr_sum < c:
+                l += 1
+            else:
+                r -= 1
+        
+        return False
