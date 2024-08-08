a549d3bfb4c1192de04ffec014e1b2a2907c1d48
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2024-08-07 23:31:58 -0400

    Time: 161 ms (87.93%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,5 +1,15 @@
-select a.machine_id, round(avg(b.timestamp - a.timestamp)::numeric, 3) as processing_time
-from activity a, activity b
-where a.machine_id = b.machine_id and a.process_id = b.process_id and a.activity_type = 'start'
-and b.activity_type = 'end'
-group by a.machine_id
+with activity_start as (
+    select machine_id, process_id, activity_type, timestamp
+    from activity
+    where activity_type = 'start'
+),
+activity_end as (
+    select machine_id, process_id, activity_type, timestamp
+    from activity
+    where activity_type = 'end'
+)
+
+select act_st.machine_id, round(avg(act_en.timestamp - act_st.timestamp)::numeric, 3) as processing_time
+from activity_start act_st
+join activity_end act_en on act_st.machine_id = act_en.machine_id and act_st.process_id = act_en.process_id
+group by act_st.machine_id
