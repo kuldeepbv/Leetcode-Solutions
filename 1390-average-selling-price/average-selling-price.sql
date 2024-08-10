aebc101bbdeee0dd349807077fbdf4ed50accfae
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2024-08-09 21:00:00 -0400

    Time: 192 ms (94.32%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,4 +1,4 @@
-select p.product_id, coalesce(round(sum(p.price * u.units)/sum(u.units)::numeric, 2), 0) as average_price
+select p.product_id, coalesce(round(sum(p.price * u.units) / sum(u.units)::numeric, 2), 0) as average_price
 from prices p
 left join unitssold u on p.product_id = u.product_id and u.purchase_date between p.start_date and p.end_date
 group by p.product_id
