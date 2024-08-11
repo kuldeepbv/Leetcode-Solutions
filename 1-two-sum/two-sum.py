5501fde334e3fe28ad6d55b9b3484ebb0914874a
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2024-08-10 21:39:33 -0400

    Time: 48 ms (96.64%) | Memory: 18 MB (7.76%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -6,7 +6,3 @@ class Solution:
                 return [map_dict[nums[i]], i]
             else:
                 map_dict[target - nums[i]] = i
-        # for i in range(len(nums)):
-        #     for j in range(i + 1, len(nums)):
-        #         if nums[j] == target - nums[i]:
-        #             return [i, j]
