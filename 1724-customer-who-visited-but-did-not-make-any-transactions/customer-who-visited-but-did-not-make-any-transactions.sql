ab0bca58b893ec3c79a04eedc4117b1dd1e348c5
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2024-08-15 23:18:08 -0400

    Time: 1292 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

--- 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,5 +1,5 @@
-select v.customer_id, count(v.customer_id) as count_no_trans
-from visits as v
-left join transactions as t on v.visit_id = t.visit_id
+select v.customer_id, count(*) as count_no_trans
+from visits v
+left join transactions t on v.visit_id = t.visit_id
 where t.transaction_id is null
 group by v.customer_id
