9e01cb7f32a5a090671f48cba7dbb73974300cfd
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2024-08-20 17:21:44 -0400

    Time: 1360 ms (37.09%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,9 +1,10 @@
-with ranked_years as (
-    select *,
-    rank() over (partition by product_id order by year) as rnk_year
+with year_ranking as (
+    select product_id, year,
+    rank() over(partition by product_id order by year) as rank_of_years,
+    quantity, price
     from sales
 )
 
-select product_id, year as first_year, quantity, price 
-from ranked_years
-where rnk_year = 1
+select product_id, year as first_year, quantity, price
+from year_ranking
+where rank_of_years = 1
