693f99342dff1803becec6a8148edb0e68e01d14
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2024-08-21 11:29:42 -0400

    Time: 38 ms (31.52%) | Memory: 16.4 MB (66.90%) - LeetSync

--- 228-summary-ranges/summary-ranges.py
+++ 228-summary-ranges/summary-ranges.py
@@ -5,7 +5,7 @@ class Solution:
 
         while i < len(nums):
             start = nums[i]  
-            while i < len(nums) - 1 and nums[i] + 1 == nums[i + 1]: 
+            while i < len(nums)-1 and nums[i] + 1 == nums[i + 1]: 
                 i += 1 
             
             if start != nums[i]: 
@@ -14,5 +14,34 @@ class Solution:
                 final.append(str(nums[i]))
             
             i += 1
-
+        
         return final
+
+        # if len(nums) == 0:
+        #     return []
+        # elif len(nums) == 1:
+        #     return [str(nums[0])]
+        # final = []
+        # start = nums[0]
+        # for i, num in enumerate(nums[1:]):
+        #     if i == len(nums) - 2:
+        #         if num == nums[i] + 1:
+        #             final.append(str(start) + '->' + str(num))
+        #             break
+        #         else:
+        #             if nums[i] == start:
+        #                 final.append(str(start))
+        #             else:
+        #                 final.append(str(start) + '->' + str(nums[i]))
+        #             final.append(str(num))
+        #             break
+        #     if num == nums[i] + 1:
+        #         continue
+        #     else:
+        #         if nums[i] == start:
+        #             final.append(str(start))
+        #         else:
+        #             final.append(str(start) + '->' + str(nums[i]))
+        #         start = num
+
+        # return final
