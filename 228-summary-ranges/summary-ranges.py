29a3d7c1600e4905d2a45ee9af468c80bfa6b4bf
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2024-08-21 11:28:15 -0400

    Time: 37 ms (39.87%) | Memory: 16.5 MB (66.90%) - LeetSync

--- 228-summary-ranges/summary-ranges.py
+++ 228-summary-ranges/summary-ranges.py
@@ -1,45 +1,18 @@
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
-        # final = []
-        # i = 0
-
-        # while i < len(nums):
-        #     start = nums[i]  
-        #     while i < len(nums)-1 and nums[i] + 1 == nums[i + 1]: 
-        #         i += 1 
-            
-        #     if start != nums[i]: 
-        #         ans.append(str(start) + "->" + str(nums[i]))
-        #     else: 
-        #         ans.append(str(nums[i]))
-            
-        #     i += 1
-
-        if len(nums) == 0:
-            return []
-        elif len(nums) == 1:
-            return [str(nums[0])]
         final = []
-        start = nums[0]
-        for i, num in enumerate(nums[1:]):
-            if i == len(nums) - 2:
-                if num == nums[i] + 1:
-                    final.append(str(start) + '->' + str(num))
-                    break
-                else:
-                    if nums[i] == start:
-                        final.append(str(start))
-                    else:
-                        final.append(str(start) + '->' + str(nums[i]))
-                    final.append(str(num))
-                    break
-            if num == nums[i] + 1:
-                continue
-            else:
-                if nums[i] == start:
-                    final.append(str(start))
-                else:
-                    final.append(str(start) + '->' + str(nums[i]))
-                start = num
+        i = 0
+
+        while i < len(nums):
+            start = nums[i]  
+            while i < len(nums) - 1 and nums[i] + 1 == nums[i + 1]: 
+                i += 1 
+            
+            if start != nums[i]: 
+                final.append(str(start) + "->" + str(nums[i]))
+            else: 
+                final.append(str(nums[i]))
+            
+            i += 1
 
         return final
