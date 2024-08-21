cce1178b531149afa8d971682a333ebac9561e53
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2024-08-21 11:26:22 -0400

    Time: 38 ms (31.52%) | Memory: 16.6 MB (26.00%) - LeetSync

--- 228-summary-ranges/summary-ranges.py
+++ 228-summary-ranges/summary-ranges.py
@@ -1,15 +1,29 @@
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
+        # final = []
+        # i = 0
+
+        # while i < len(nums):
+        #     start = nums[i]  
+        #     while i < len(nums)-1 and nums[i] + 1 == nums[i + 1]: 
+        #         i += 1 
+            
+        #     if start != nums[i]: 
+        #         ans.append(str(start) + "->" + str(nums[i]))
+        #     else: 
+        #         ans.append(str(nums[i]))
+            
+        #     i += 1
+
         if len(nums) == 0:
             return []
         elif len(nums) == 1:
             return [str(nums[0])]
         final = []
         start = nums[0]
-        temp = start
         for i, num in enumerate(nums[1:]):
             if i == len(nums) - 2:
-                if num == temp + 1:
+                if num == nums[i] + 1:
                     final.append(str(start) + '->' + str(num))
                     break
                 else:
@@ -19,8 +33,7 @@ class Solution:
                         final.append(str(start) + '->' + str(nums[i]))
                     final.append(str(num))
                     break
-            if num == temp + 1:
-                temp = num
+            if num == nums[i] + 1:
                 continue
             else:
                 if nums[i] == start:
@@ -28,6 +41,5 @@ class Solution:
                 else:
                     final.append(str(start) + '->' + str(nums[i]))
                 start = num
-                temp = start
 
         return final
