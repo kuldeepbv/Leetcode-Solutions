fd9b2c150c0687ca5959ebdacd5752b5c110de94
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2024-08-22 10:51:09 -0400

    Time: 333 ms (77.39%) | Memory: 0B (100.00%) - LeetSync

--- 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -1,15 +1,11 @@
 with managers as (
-    select reports_to, count(reports_to) as reports_count, round(avg(age)) as average_age
-    from employees
+    select reports_to, age 
+    from employees 
     where reports_to is not null
-    group by reports_to
 )
 
-select m.reports_to as employee_id, e.name, m.reports_count, m.average_age
+select e.employee_id, e.name, count(*) as reports_count, round(avg(m.age)) as average_age
 from managers m
-join employees e on m.reports_to = e.employee_id
-order by employee_id
-
---select * from managers
---select employee_id, name, reports_count
---from employees
+left join employees e on m.reports_to = e.employee_id
+group by e.employee_id, e.name
+order by e.employee_id
