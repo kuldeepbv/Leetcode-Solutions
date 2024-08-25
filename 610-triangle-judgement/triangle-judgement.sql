4e0567166970c256d3fcd2886ad8f22432b1a1cc
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2024-08-24 23:54:25 -0400

    Time: 198 ms (57.33%) | Memory: 0B (100.00%) - LeetSync

--- 610-triangle-judgement/triangle-judgement.sql
+++ 610-triangle-judgement/triangle-judgement.sql
@@ -1,2 +1,2 @@
-select x, y, z, (case when (x + y + z - greatest(x, y, z)) > greatest(x, y, z) then 'Yes' else 'No' end) as triangle
-from triangle 
+select *, (case when (x + y > z) and (x + z > y) and (z + y > x) then 'Yes' else 'No' end) as triangle
+from triangle
