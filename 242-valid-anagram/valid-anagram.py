52c892495b762a09a49e3d8f159382bff22cf1e9
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2024-08-29 11:01:45 -0400

    Time: 49 ms (56.86%) | Memory: 16.9 MB (46.97%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,12 +1,20 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        s_dict = {}
+        t_dict = {}
         for char in s:
-            if char not in t:
-                return False
+            if char not in s_dict:
+                s_dict[char] = 1
             else:
-                t = t.replace(char, '', 1)
+                s_dict[char] += 1
+        
+        for char in t:
+            if char not in t_dict:
+                t_dict[char] = 1
+            else:
+                t_dict[char] += 1
 
-        if t == '':
+        if s_dict == t_dict:
             return True
         else:
             return False
