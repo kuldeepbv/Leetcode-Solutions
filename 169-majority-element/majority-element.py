9ed3c89c4c8760308b0d1206431db44bf2e69f73
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2024-08-30 13:22:02 -0400

    Time: 160 ms (93.21%) | Memory: 17.9 MB (79.15%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,12 +1,15 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = {}
-        for num in nums:
-            if num not in count.keys():
-                count[num] = 1
-            else:
-                count[num] += 1
+        nums.sort()
+        n = len(nums)
+        return nums[n//2]
+        # count = {}
+        # for num in nums:
+        #     if num not in count.keys():
+        #         count[num] = 1
+        #     else:
+        #         count[num] += 1
         
-        for key, value in count.items():
-            if value > (len(nums) / 2):
-                return key
+        # for key, value in count.items():
+        #     if value > (len(nums) / 2):
+        #         return key
