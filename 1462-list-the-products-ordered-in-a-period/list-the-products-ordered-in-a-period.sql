d81f709232ca81d1f64b17383d6e8db471421116
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2024-09-01 15:14:48 -0400

    Time: 254 ms (39.46%) | Memory: 0B (100.00%) - LeetSync

--- 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -14,4 +14,9 @@ from products p
 join sum_of_orders so on p.product_id = so.product_id
 where so.unit >= 100
 
---select 
+-- select p.product_name, sum(o.unit) as unit
+-- from products p
+-- join orders o on p.product_id = o.product_id
+-- where to_char(order_date, 'MM-YYYY') = '02-2020'
+-- group by p.product_name
+-- --having unit >= 100
