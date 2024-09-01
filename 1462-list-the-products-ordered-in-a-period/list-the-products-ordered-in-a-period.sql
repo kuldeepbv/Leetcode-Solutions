936f959cf4f5524e06dd3f685cd772526ef10bb3
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2024-09-01 15:15:32 -0400

    Time: 198 ms (98.21%) | Memory: 0B (100.00%) - LeetSync

--- 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -1,22 +1,22 @@
-with month_orders as (
-    select product_id, to_char(order_date, 'MM-YYYY') as month_year, unit
-    from orders
-),
-sum_of_orders as (
-    select product_id, sum(unit) as unit
-    from month_orders
-    where month_year = '02-2020'
-    group by product_id
-)
+-- with month_orders as (
+--     select product_id, to_char(order_date, 'MM-YYYY') as month_year, unit
+--     from orders
+-- ),
+-- sum_of_orders as (
+--     select product_id, sum(unit) as unit
+--     from month_orders
+--     where month_year = '02-2020'
+--     group by product_id
+-- )
 
-select p.product_name, so.unit
-from products p
-join sum_of_orders so on p.product_id = so.product_id
-where so.unit >= 100
-
--- select p.product_name, sum(o.unit) as unit
+-- select p.product_name, so.unit
 -- from products p
--- join orders o on p.product_id = o.product_id
--- where to_char(order_date, 'MM-YYYY') = '02-2020'
--- group by p.product_name
--- --having unit >= 100
+-- join sum_of_orders so on p.product_id = so.product_id
+-- where so.unit >= 100
+
+select p.product_name, sum(o.unit) as unit
+from products p
+join orders o on p.product_id = o.product_id
+where to_char(order_date, 'MM-YYYY') = '02-2020'
+group by p.product_name
+having sum(o.unit) >= 100
