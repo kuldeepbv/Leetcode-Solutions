6aa620bbe40f09d2c9665332bf001618a346d924
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2024-09-05 12:14:59 -0400

    Time: 546 ms (93.08%) | Memory: 20.7 MB (72.59%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,24 +1,46 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        final = []
         nums.sort()
-
-        for i in range(len(nums)-2):
-            if i>0 and nums[i] == nums[i-1]:
+        final = []
+        for i, num in enumerate(nums):
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
-            target = nums[i]
-            j = i + 1
-            k = len(nums) - 1
-            while j < k:
-                if nums[j] + nums[k] == -target:
-                    final.append([nums[i], nums[j], nums[k]])
-                    j += 1
-                    while nums[j] == nums[j-1] and j < k:
-                        j += 1
-                    #k -= 1
-                elif nums[i] + nums[j] + nums[k] > 0:
-                    k -= 1
-                else:
-                    j += 1
+            target = -1 * num
+            left = i + 1
+            right = len(nums) - 1
+            while left < right:
+                temp_target = nums[left] + nums[right]
+                
+                if temp_target > target:
+                    right -= 1
+                
+                elif temp_target < target:
+                    left += 1
 
+                else:
+                    final.append([num, nums[left], nums[right]])
+                    left += 1
+
+                    while nums[left] == nums[left - 1] and left < right:
+                        left += 1
+            
         return final
+        #     while left < right:
+        #         temp_target = nums[left] + nums[right]
+        #         if temp_target == target:
+        #             temp = [num, nums[left], nums[right]]
+        #             temp.sort
+        #             if temp not in final:
+        #                 final.append(temp)
+        #             left += 1
+        #             right -= 1
+        #             while left < right and nums[left] == nums[left-1]:
+        #                 left += 1
+        #             while left < right and nums[right] == nums[right+1]:
+        #                 right -= 1
+        #         elif temp_target < target:
+        #             left += 1
+        #         else:
+        #             right -= 1
+
+        # return final
