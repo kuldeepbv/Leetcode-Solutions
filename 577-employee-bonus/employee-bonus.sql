f473aec02d1ac3cc51135f643565f64ea50f2bc5
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2024-11-22 13:27:39 -0500

    Time: 440 ms (22.14%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -1,4 +1,4 @@
 select e.name, b.bonus
-from employee as e
-left join bonus as b on e.empid = b.empid
+from employee e
+left join bonus b on e.empid = b.empid
 where b.bonus < 1000 or b.bonus is null
