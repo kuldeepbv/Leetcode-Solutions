f403a78b34a40778dafee7e95ab9b522b7e5a11e
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2024-11-26 16:28:27 -0500

    Time: 263 ms (55.00%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,4 +1,4 @@
-select query_name, round(sum(rating * 1.00 / position)/count(*)::numeric, 2) as quality, round(sum(case when rating < 3 then 1 else 0 end) * 100 / count(*)::numeric, 2) as poor_query_percentage
+select query_name, round(sum(rating * 1.0 / position) / count(*)::numeric, 2) as quality, round(sum(case when rating < 3 then 1 else 0 end) * 100 / count(*)::numeric, 2) as poor_query_percentage
 from queries
 where query_name is not null
 group by query_name
