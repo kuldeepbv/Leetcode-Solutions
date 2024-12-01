4e37af07f1dbe06e4c54ad6237d72a537d28d2eb
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2024-12-01 16:57:41 -0500

    Time: 254 ms (65.39%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,19 +1,10 @@
-with department_count as (
-    select employee_id, count(*) as departments
+with dept_count as (
+    select employee_id, count(*) as cnt
     from employee
     group by employee_id
 )
--- final_query as (
---     select dc.employee_id, (case when dc.departments = 1 then e.    department_id else case when e.primary_flag = 'Y' then e.department_id end end) as department_id
---     from department_count dc
---     join employee e on dc.employee_id = e.employee_id
--- )
-
--- select *
--- from final_query
--- where department_id is not null
 
 select dc.employee_id, e.department_id
-from department_count dc
+from dept_count dc
 join employee e on dc.employee_id = e.employee_id
-where (dc.departments = 1 or e.primary_flag = 'Y') and e.department_id is not null
+where (dc.cnt = 1 or e.primary_flag = 'Y')
