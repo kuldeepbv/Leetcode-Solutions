da0b70bde9de6d0bd9ec402552802fd8de14d1e2
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2024-12-29 16:35:09 -0500

    Time: 407 ms (15.21%) | Memory: 0B (100.00%) - LeetSync

--- 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -1,4 +1,4 @@
 select employee_id
 from employees
-where manager_id not in (select distinct employee_id from employees) and salary < 30000
+where salary < 30000 and manager_id not in (select distinct employee_id from employees)
 order by employee_id
