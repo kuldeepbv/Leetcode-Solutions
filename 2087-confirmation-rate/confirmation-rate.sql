2d5221fa088e379b49cfd2395800fe7b168c547a
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2025-04-21 22:06:40 -0400

    Time: 260 ms (69.55%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,5 +1,4 @@
-select s.user_id, 
-round(sum(case when c.action = 'confirmed' then 1 else 0 end)/count(*)::numeric, 2) as confirmation_rate
+select s.user_id, round(avg(case when action = 'confirmed' then 1 else 0 end)::numeric, 2) as confirmation_rate
 from signups s
 left join confirmations c on s.user_id = c.user_id
 group by s.user_id
