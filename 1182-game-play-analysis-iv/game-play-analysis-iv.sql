fd0505a1dceb991a32108328039ed0a0916c9099
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2025-04-22 17:57:48 -0400

    Time: 677 ms (47.10%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,9 +1,9 @@
-with ranked_login as (
-    select player_id, event_date, 
-    rank() over(partition by player_id order by event_date) as login_rank
+with log_in_rank as (
+    select player_id, event_date,
+    rank() over(partition by player_id order by event_date) as rnk
     from activity
 )
 
-select round(sum(case when rl1.event_date + 1 = rl2.event_date then 1 else 0 end) / (select count(distinct player_id) from activity)::numeric, 2) as fraction
-from ranked_login rl1
-left join ranked_login rl2 on rl1.player_id = rl2.player_id and rl1.login_rank = 1 and rl2.login_rank = 2
+select round(count(*) / (select count(distinct player_id) from activity)::numeric, 2) as fraction
+from log_in_rank lir1
+join log_in_rank lir2 on lir1.player_id = lir2.player_id and lir1.rnk = 1 and lir2.rnk = 2 and lir2.event_date - lir1.event_date = 1
