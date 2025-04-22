02c39f7d40461354b1d478bd5c2816aa42863acc
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2025-04-22 18:15:28 -0400

    Time: 353 ms (87.00%) | Memory: 0B (100.00%) - LeetSync

--- 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -1,4 +1,12 @@
-select activity_date as day, count(distinct user_id) as active_users
-from activity
-where activity_date between '2019-06-28' and '2019-07-27'
-group by activity_date
+with users as (
+    select user_id, session_id
+    from activity
+    group by user_id, session_id
+    having count(*) > 0
+)
+
+select a.activity_date as day, count(distinct a.user_id) as active_users
+from activity a
+join users u on a.user_id = u.user_id and a.session_id = u.session_id
+where a.activity_date between '2019-06-28' and '2019-07-27'
+group by a.activity_date
