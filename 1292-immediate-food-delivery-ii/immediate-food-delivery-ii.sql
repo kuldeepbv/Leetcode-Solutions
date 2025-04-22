c38272ab0953f2c8bca2312d82c2ed9f08fe2fbc
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2025-04-22 12:43:13 -0400

    Time: 512 ms (51.63%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,9 +1,9 @@
-with order_nums as (
+with order_rnk as (
     select *,
-    rank() over(partition by customer_id order by order_date) as order_num
+    rank() over(partition by customer_id order by order_date) as rnk
     from delivery
 )
 
-select round(sum(case when order_date = customer_pref_delivery_date then 1 else 0 end) * 100 / count(*)::numeric, 2) as immediate_percentage
-from order_nums
-where order_num = 1
+select round(count(*) * 100.0 / (select count(distinct customer_id) from delivery)::numeric, 2) as immediate_percentage
+from order_rnk
+where order_date = customer_pref_delivery_date and rnk = 1
