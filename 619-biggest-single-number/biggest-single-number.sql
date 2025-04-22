ef7cc47afcb552fe909ef36cfe0bb518e2dc0aed
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2025-04-22 18:39:21 -0400

    Time: 196 ms (78.79%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,2 +1,9 @@
+with single_num as (
+    select num
+    from mynumbers
+    group by num
+    having count(*) = 1
+)
+
 select max(num) as num
-from (select * from mynumbers group by num having count(*) = 1)
+from single_num
