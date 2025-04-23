7f2416f64f470682ede4f46a2367631fc25cde2d
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2025-04-23 00:59:21 -0400

    Time: 592 ms (54.94%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,10 +1,31 @@
+-- with cum_sum as (
+--     select person_id, person_name, turn, sum(weight) over (order by turn) as added_sum
+--     from queue
+-- )
+
+-- select person_name 
+-- from cum_sum
+-- where added_sum <= 1000
+-- order by turn desc
+-- limit 1
+
+
+
+
+
+
+
+
+
+
 with cum_sum as (
-    select person_id, person_name, turn, sum(weight) over (order by turn) as added_sum
+    select person_name, turn,
+    sum(weight) over(order by turn) as cum_weight
     from queue
 )
 
-select person_name 
+select person_name
 from cum_sum
-where added_sum <= 1000
+where cum_weight <= 1000
 order by turn desc
 limit 1
