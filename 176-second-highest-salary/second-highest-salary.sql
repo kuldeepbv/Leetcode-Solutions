6346db0324e1e71559ff7a31866eb2c722a8053f
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2025-05-26 17:00:01 -0400

    Time: 194 ms (86.09%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,12 +1,21 @@
-select max(salary) as secondhighestsalary 
+select max(salary) as SecondHighestSalary
 from employee
 where salary < (select max(salary) from employee)
 
--- with highest_salary as (
---     select id, rank() over(order by salary desc) as rnk
---     from employee
--- )
 
--- select max(salary) as secondhighestsalary
+
+
+
+
+-- select max(salary) as secondhighestsalary 
 -- from employee
--- where id not in (select id from highest_salary where rnk = 1)
+-- where salary < (select max(salary) from employee)
+
+-- -- with highest_salary as (
+-- --     select id, rank() over(order by salary desc) as rnk
+-- --     from employee
+-- -- )
+
+-- -- select max(salary) as secondhighestsalary
+-- -- from employee
+-- -- where id not in (select id from highest_salary where rnk = 1)
