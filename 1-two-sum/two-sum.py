e4329072c844b7757b985dd4fa2a00186f9cc8ae
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2025-05-29 17:24:59 -0400

    Time: 4 ms (41.38%) | Memory: 18.8 MB (62.75%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,8 +1,18 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        map_dict = {}
+        hashmap = {}
+
         for i in range(len(nums)):
-            if nums[i] in map_dict:
-                return [map_dict[nums[i]], i]
-            else:
-                map_dict[target - nums[i]] = i
+            complement = target - nums[i]
+            if complement in hashmap:
+                return [i, hashmap[complement]]
+            hashmap[nums[i]] = i
+        return []
+
+
+        # map_dict = {}
+        # for i in range(len(nums)):
+        #     if nums[i] in map_dict:
+        #         return [map_dict[nums[i]], i]
+        #     else:
+        #         map_dict[target - nums[i]] = i
