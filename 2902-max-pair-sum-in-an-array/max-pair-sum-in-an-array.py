b56bbf7ff298191d93eef2de0f94461788b5ab16
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2025-06-23 13:19:36 -0400

    Time: 32 ms (57.48%) | Memory: 18 MB (18.77%) - LeetSync

---
+++ 2902-max-pair-sum-in-an-array/max-pair-sum-in-an-array.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxSum(self, nums: List[int]) -> int:
+        nums_dict = {}
+
+        for num in nums:
+            digits = [int(dig) for dig in str(num)]
+            max_dig = max(digits)
+            if max_dig in nums_dict:
+                nums_dict[max_dig].append(num)
+            else:
+                nums_dict[max_dig] = [num]
+
+        final = []
+
+        for key, value in nums_dict.items():
+            if len(value) >= 2:
+                sorted_list = sorted(value, reverse = True)
+                final.append(sorted_list[0] + sorted_list[1])
+
+        if len(final) == 0:
+            return -1
+        else:
+            return max(final)
