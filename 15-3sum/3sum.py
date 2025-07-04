d013740aab3ab5d65624cb76d66552a312498429
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2025-07-04 14:49:34 -0400

    Time: 539 ms (74.85%) | Memory: 20.7 MB (36.53%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,46 +1,68 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
+        # nums.sort()
+        # final = []
+        # for i, num in enumerate(nums):
+        #     if i > 0 and nums[i] == nums[i-1]:
+        #         continue
+        #     target = -1 * num
+        #     left = i + 1
+        #     right = len(nums) - 1
+        #     while left < right:
+        #         temp_target = nums[left] + nums[right]
+                
+        #         if temp_target > target:
+        #             right -= 1
+                
+        #         elif temp_target < target:
+        #             left += 1
+
+        #         else:
+        #             final.append([num, nums[left], nums[right]])
+        #             left += 1
+
+        #             while nums[left] == nums[left - 1] and left < right:
+        #                 left += 1
+            
+        # return final
+
         final = []
+        nums.sort()
+
         for i, num in enumerate(nums):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
             target = -1 * num
-            left = i + 1
-            right = len(nums) - 1
-            while left < right:
-                temp_target = nums[left] + nums[right]
-                
-                if temp_target > target:
-                    right -= 1
-                
-                elif temp_target < target:
-                    left += 1
-
+            l = i + 1
+            r = len(nums) - 1
+            while l < r:
+                if nums[l] + nums[r] < target:
+                    l += 1
+                elif nums[l] + nums[r] > target:
+                    r -= 1
                 else:
-                    final.append([num, nums[left], nums[right]])
-                    left += 1
+                    final.append([num, nums[l], nums[r]])
+                    l += 1
 
-                    while nums[left] == nums[left - 1] and left < right:
-                        left += 1
-            
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+        
         return final
-        #     while left < right:
-        #         temp_target = nums[left] + nums[right]
-        #         if temp_target == target:
-        #             temp = [num, nums[left], nums[right]]
-        #             temp.sort
+
+
+
+        # final = []
+        # for i, num in enumerate(nums):
+        #     target = -1 * num
+        #     map_dict = {}
+        #     for new_num in nums[i+1:]:
+        #         temp_target = target - new_num
+        #         if temp_target not in map_dict:
+        #             map_dict[new_num] = new_num
+        #         else:
+        #             temp = [num, new_num, temp_target]
+        #             temp.sort()
         #             if temp not in final:
         #                 final.append(temp)
-        #             left += 1
-        #             right -= 1
-        #             while left < right and nums[left] == nums[left-1]:
-        #                 left += 1
-        #             while left < right and nums[right] == nums[right+1]:
-        #                 right -= 1
-        #         elif temp_target < target:
-        #             left += 1
-        #         else:
-        #             right -= 1
-
+        
         # return final
