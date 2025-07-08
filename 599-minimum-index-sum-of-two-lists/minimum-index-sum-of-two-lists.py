b8671ab5ee27b9d368f7fe3d27eac874be1ead50
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2025-07-07 23:56:24 -0400

    Time: 109 ms (17.97%) | Memory: 18.4 MB (26.35%) - LeetSync

--- 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.py
+++ 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.py
@@ -3,14 +3,10 @@ class Solution:
         final = []
         temp = len(list1) + len(list2)
         for i, word in enumerate(list1):
-            print(word)
             if word in list2:
-                print('1st if')
                 if i + list2.index(word) < temp:
-                    print('2nd if')
                     final = [word]
                     temp =  i + list2.index(word)
                 elif i + list2.index(word) == temp:
-                    print('elif')
                     final.append(word)
         return final
