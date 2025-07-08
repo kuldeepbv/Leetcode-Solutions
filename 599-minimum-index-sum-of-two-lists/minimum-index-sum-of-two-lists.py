8082f41466a9f9615a833cd4a83017a4e50ce29c
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2025-07-07 23:57:40 -0400

    Time: 99 ms (21.27%) | Memory: 18.2 MB (70.22%) - LeetSync

--- 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.py
+++ 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.py
@@ -1,7 +1,7 @@
 class Solution:
     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
         final = []
-        temp = 2001
+        temp = len(list1) + len(list2)
         for i, word in enumerate(list1):
             if word in list2:
                 if i + list2.index(word) < temp:
