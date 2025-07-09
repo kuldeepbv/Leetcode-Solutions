e8e4ebc9c5d5885ff6cad9a8970563507474d4b9
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2025-07-09 00:55:46 -0400

    Time: 6 ms (86.34%) | Memory: 19.3 MB (40.75%) - LeetSync

--- 645-set-mismatch/set-mismatch.py
+++ 645-set-mismatch/set-mismatch.py
@@ -1,14 +1,20 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        final = {}
-        result = []
-        for num in nums:
-            if num not in final:
-                final[num] = 1
-            else:
-                result.append(num)
+        l = len(nums)
+        expec_sum = (l * (l + 1)) // 2
+        curr_sum = sum(nums)
+        unique_sum = sum(set(nums))
 
-        for i in range(len(nums)):
-            if i + 1 not in final:
-                result.append(i+1)
-                return result
+        return [curr_sum - unique_sum, expec_sum - unique_sum]
+        # final = {}
+        # result = []
+        # for num in nums:
+        #     if num not in final:
+        #         final[num] = 1
+        #     else:
+        #         result.append(num)
+
+        # for i in range(len(nums)):
+        #     if i + 1 not in final:
+        #         result.append(i+1)
+        #         return result
