273731f113b0a703998c2f3dc1f53d882fa24014
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2025-07-09 23:51:41 -0400

    Time: 45 ms (84.52%) | Memory: 30 MB (34.38%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.py
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.py
@@ -1,14 +1,34 @@
 class Solution:
     def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
         arr.sort()
-        diff = []
-        for i in range(len(arr) - 1):
-            diff.append(abs(arr[i] - arr[i+1]))
+        minDiff = arr[1] - arr[0]
+        count = 0
+        ans = []
         
-        min_diff = min(diff)
-        final = []
-        for i, num in enumerate(diff):
-            if num == min_diff:
-                final.append([arr[i], arr[i+1]])
+        for i in range(len(arr)-1):
+            currDiff = arr[i+1] - arr[i]
+            
+            if currDiff < minDiff:
+                minDiff = currDiff
+                count = 1
+                ans = [[arr[i], arr[i+1]]]
+            elif currDiff == minDiff:
+                count += 1
+                ans.append([arr[i], arr[i+1]])
 
-        return final
+            else:
+                continue
+        
+        return ans
+        # arr.sort()
+        # diff = []
+        # for i in range(len(arr) - 1):
+        #     diff.append(abs(arr[i] - arr[i+1]))
+        
+        # min_diff = min(diff)
+        # final = []
+        # for i, num in enumerate(diff):
+        #     if num == min_diff:
+        #         final.append([arr[i], arr[i+1]])
+
+        # return final
