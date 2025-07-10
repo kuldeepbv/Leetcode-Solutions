658080f5bcbcc2abff7c5c1e8587cccf621e785d
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2025-07-10 00:46:44 -0400

    Time: 1 ms (18.13%) | Memory: 17.6 MB (83.28%) - LeetSync

--- 1848-sum-of-unique-elements/sum-of-unique-elements.py
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.py
@@ -4,20 +4,10 @@ class Solution:
         repeat = []
         for num in nums:
             if num in repeat:
-                print('1st if')
                 continue
             if num not in ans:
-                print('2nd if')
                 ans.append(num)
             else:
-                print('else')
                 repeat.append(num)
                 ans.remove(num)
-            print(ans)
-            print(repeat)
         return sum(ans)
-        #     if num not in 
-        #     if num not in nums[i+1:]:
-        #         print('if')
-        #         ans += num
-        # return ans
