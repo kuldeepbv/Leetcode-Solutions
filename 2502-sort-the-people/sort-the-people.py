9ee62420a2a59026c58fa8da256ff22962f8e617
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2025-07-10 17:42:07 -0400

    Time: 8 ms (25.47%) | Memory: 18.5 MB (9.78%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,9 +1,20 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        ans = []
-        sorted_heights = sorted(heights, reverse = True)
+        # ans = []
+        # sorted_heights = sorted(heights, reverse = True)
 
-        for h in sorted_heights:
-            idx = heights.index(h)
-            ans.append(names[idx])
+        # for h in sorted_heights:
+        #     idx = heights.index(h)
+        #     ans.append(names[idx])
+        # return ans
+
+        # ans = {}
+        # for name, height in zip(names, heights):
+        #     ans[name] = height
+        # sort_dict = sorted(ans.items(), key = lambda item: item[1], reverse = True)
+        l = list(zip(names, heights))
+        sort_l = sorted(l, key = lambda item: item[1], reverse = True)
+        ans = []
+        for nh in sort_l:
+            ans.append(nh[0])
         return ans
