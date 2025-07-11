c02e1f32f5ff33d00ded2e7341dae483b853b543
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2025-07-10 23:00:43 -0400

    Time: 254 ms (47.00%) | Memory: 17.9 MB (62.33%) - LeetSync

--- 2831-number-of-beautiful-pairs/number-of-beautiful-pairs.py
+++ 2831-number-of-beautiful-pairs/number-of-beautiful-pairs.py
@@ -1,4 +1,3 @@
-from math import gcd
 class Solution:
     def countBeautifulPairs(self, nums: List[int]) -> int:
         ans = 0
@@ -6,6 +5,11 @@ class Solution:
             f = int(str(nums[i])[0])
             for next_num in nums[i+1:]:
                 l = int(str(next_num)[-1])
-                if gcd(f, l) == 1:
+                if self.gcd(f, l) == 1:
                     ans += 1
         return ans
+
+    def gcd(self, x, y):
+        while y:
+            x, y = y, x % y
+        return x
