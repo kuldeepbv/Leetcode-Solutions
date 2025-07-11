7ed9aabf7ba28ba4916d52ece30625147242f9b3
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2025-07-11 19:21:29 -0400

    Time: 41 ms (6.88%) | Memory: 17.9 MB (71.38%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.py
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.py
@@ -3,6 +3,6 @@ class Solution:
         set_nums = list(set(nums))
         ans = 0
         for num in set_nums:
-            if nums.count(num) % 2 == 0:
-                ans += 1
-        return ans == len(set_nums)
+            if nums.count(num) % 2 != 0:
+                return False
+        return True
