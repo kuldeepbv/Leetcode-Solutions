a7b0330061e7c27d3548dfa9f006015d0f642cf9
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2025-07-13 10:43:47 -0400

    Time: 3 ms (5.48%) | Memory: 18 MB (12.62%) - LeetSync

--- 2536-determine-if-two-events-have-conflict/determine-if-two-events-have-conflict.py
+++ 2536-determine-if-two-events-have-conflict/determine-if-two-events-have-conflict.py
@@ -12,16 +12,3 @@ class Solution:
             if time1 in range(min_time2, max_time2 + 1):
                 return True
         return False
-
-
-        # min_time = min(event1[0], event2[0])
-        # min_time = int(min_time.split(':')[0]) * 60 + int(min_time.split(':')[1])
-        
-        # max_time = max(event1[1], event2[1])
-        # max_time = int(max_time.split(':')[0]) * 60 + int(max_time.split(':')[1])
-
-        # for i in range(min_time, max_time):
-        #     hr = i // 60
-        #     mi = i % 60
-            
-            #print(i)
