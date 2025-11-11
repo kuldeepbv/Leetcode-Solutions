7a92eacac61fa659c8d5670d8f1d45858636d050
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2025-11-11 17:50:43 -0500

    Time: 240 ms (83.52%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,3 +1,21 @@
 select name
 from customer
 where referee_id != 2 or referee_id is null
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- select name
+-- from customer
+-- where referee_id != 2 or referee_id is null
