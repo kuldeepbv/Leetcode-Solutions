90a0edbb872db96c0a45ab4a26f9a576317921b7
Kuldeep Bhurabhai Vadhiya <123977771+kuldeepbv@users.noreply.github.com>
2025-11-11 18:05:55 -0500

    Time: 210 ms (86.26%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,3 +1,3 @@
-select w2.id
-from weather w1, weather w2
-where w2.recorddate - w1.recorddate = 1 and w2.temperature > w1.temperature
+select w1.id
+from weather w1
+join weather w2 on w2.recorddate = w1.recorddate - 1 and w1.temperature > w2.temperature
